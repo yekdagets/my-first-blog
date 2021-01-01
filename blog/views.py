21f8cbda1b5f3e90cbd6f8e2bb8d212c645402fd
yekda <yekda@yekda.com>
2021-01-01 19:07:58 +0300

    Modified tmeplates- mix HTML & tags

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 
 def post_list(request):
-    return render(request,'blog/post_list.html',{})
+    posts=Post.objects.all()
+    return render(request,'blog/post_list.html',{'posts':posts})
